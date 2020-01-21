44ab9e6763a6706e2e98d1decb1cff5c8cfdf8cb
yousif diyhaa <yousif7733@gmail.com>
2020-01-21 19:36:59 +0000

    Automatically backed up by Learn

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import moment from 'moment';
 import ExampleComponent from './ExampleComponent'
-import TestComponent from './TestComponent'
 
 // Add your code own within the return statement
 
